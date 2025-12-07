2d731880752ea5dfa6e107d2afbf690ebaf74af6
Gurleen <gurleen05012005@gmail.com>
2025-12-07 17:06:23 +0530

    LMS

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/Webhooks.js'
+
+
+// Initialize Express
+const app = express()
+
+// Connect to Database
+await connectDB()
+
+// Middleware
+app.use(cors())
+
+//Routes
+app.get('/', (req, res)=> res.send("API Working"))
+app.post('/clerk', express.json(), clerkWebhooks)
+
+// Port
+const PORT = process.env.port || 5000
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on  port ${PORT}`)
+})
